71f356d4656f95021f510f81ec0a08ddac894987
Bryan Moyles <bmoyles@google.com>
2017-06-22 13:02:51 -0400

    Minimizing the collectd configuration and adjusting for kubernetes support.

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,26 @@
+#!/bin/bash
+
 configuration_file="/opt/stackdriver/collectd/etc/collectd.conf"
 
-if [ -f /usr/src/collectd/collectd.conf ]; then
-    configuration_file="/usr/src/collectd/collectd.conf"
+if [[ ! -z "$NODE_NAME" ]]; then
+    sed -i "s/%METADATA_AGENT_URL%/$NODE_NAME/" "$configuration_file"
+else
+    sed -i "s/%METADATA_AGENT_URL%/metadata-agent/" "$configuration_file"
 fi
 
-echo "Using configuration file: $configuration_file."
+if [[ ! -z "$POD_NAMESPACE"  &&  ! -z "$POD_NAME"  &&  ! -z "$MONITORED_CONTAINER_NAME" ]]; then
+    sed -i "s/%HOSTNAME%/gke_container.$POD_NAMESPACE.$POD_NAME.$MONITORED_CONTAINER_NAME/" "$configuration_file"
+
+elif [[ -e /var/run/docker.sock ]]; then
+    iid=$(curl --silent -f -H 'Metadata-Flavor: Google' http://169.254.169.254/computeMetadata/v1/instance/id 2>/dev/null)
+
+    sed -i "s/%HOSTNAME%/$iid/" "$configuration_file"
+
+else
+    echo "Failed to determine the monitored resource."
+    exit 1
+
+fi
 
 mount -a && \
     cp /etc/hosts /host/etc/hosts && \
