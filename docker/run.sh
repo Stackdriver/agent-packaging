08c5a7fa650cb63d1501d02e1e1988834076fca0
Bryan Moyles <bmoyles@google.com>
2017-06-30 15:49:44 -0400

    Copy entire /etc/ folder into /host/etc and simplify the run.sh.

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 configuration_file="/opt/stackdriver/collectd/etc/collectd.conf"
+container_id=$(cat /proc/self/cgroup | grep -o  -e "docker/.*" | head -n 1 | sed "s/docker\/\(.*\)/\\1/")
 
 if [[ ! -z "$NODE_NAME" ]]; then
     sed -i "s/%METADATA_AGENT_URL%/$NODE_NAME/" "$configuration_file"
@@ -8,21 +9,25 @@ else
     sed -i "s/%METADATA_AGENT_URL%/metadata-agent/" "$configuration_file"
 fi
 
+monitored_resource=""
+
 if [[ ! -z "$POD_NAMESPACE"  &&  ! -z "$POD_NAME"  &&  ! -z "$MONITORED_CONTAINER_NAME" ]]; then
-    sed -i "s/%HOSTNAME%/gke_container.$POD_NAMESPACE.$POD_NAME.$MONITORED_CONTAINER_NAME/" "$configuration_file"
+    monitored_resource="gke_containerName.$POD_NAMESPACE.$POD_NAME.$MONITORED_CONTAINER_NAME"
+
+elif [[ ! -z "$container_id" ]]; then
+    monitored_resource="container.$container_id"
 
 elif [[ -e /var/run/docker.sock ]]; then
-    iid=$(curl --silent -f -H 'Metadata-Flavor: Google' http://169.254.169.254/computeMetadata/v1/instance/id 2>/dev/null)
-
-    sed -i "s/%HOSTNAME%/$iid/" "$configuration_file"
-
-else
-    echo "Failed to determine the monitored resource."
-    exit 1
-
+    monitored_resource=$(curl --silent -f -H 'Metadata-Flavor: Google' http://169.254.169.254/computeMetadata/v1/instance/id 2>/dev/null)
 fi
 
+if [[ -z "$monitored_resource" ]]; then
+    echo "Failed to determine the monitored resource."
+    exit 1
+fi
+
+sed -i "s/%HOSTNAME%/$monitored_resource/" "$configuration_file"
+
 mount -a && \
-    cp /etc/hosts /host/etc/hosts && \
-    cp /etc/resolv.conf /host/etc/resolv.conf && \
+    cp -r /etc/* /host/etc/ && \
     chroot /host /opt/stackdriver/collectd/sbin/stackdriver-collectd -f -C "$configuration_file"
