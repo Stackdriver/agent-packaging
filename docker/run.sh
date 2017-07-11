878f17c8acc7c4371a2d158b2ce17eb5d3498206
Bryan Moyles <bmoyles@google.com>
2017-07-11 12:49:09 -0400

    Fix monitored_resource in run.sh and add stackdriver agent resource id.

--- docker/run.sh
+++ docker/run.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
 configuration_file="/opt/stackdriver/collectd/etc/collectd.conf"
+
+# It is OK if the container_id is empty, the monitoring agent will attempt to fill it.
 container_id=$(cat /proc/self/cgroup | grep -o  -e "docker/.*" | head -n 1 | sed "s/docker\/\(.*\)/\\1/")
 
-if [[ ! -z "$NODE_NAME" ]]; then
-    sed -i "s/%METADATA_AGENT_URL%/$NODE_NAME/" "$configuration_file"
-else
-    sed -i "s/%METADATA_AGENT_URL%/metadata-agent/" "$configuration_file"
+sed -i "s/%STACKDRIVER_AGENT_RESOURCE_ID%/$container_id/" "$configuration_file"
+
+# If the metadata agent hostname is not provided, we will assume a link by the name of metadata-agent.
+if [[ ! -z "$METADATA_AGENT_HOSTNAME" ]]; then
+    METADATA_AGENT_HOSTNAME="metadata-agent"
 fi
 
+sed -i "s/%METADATA_AGENT_HOSTNAME%/$METADATA_AGENT_HOSTNAME/" "$configuration_file"
+
+# Determine the monitored resource based or use empty and let the monitoring agent figure it out.
 monitored_resource=""
 
 if [[ ! -z "$POD_NAMESPACE"  &&  ! -z "$POD_NAME"  &&  ! -z "$MONITORED_CONTAINER_NAME" ]]; then
     monitored_resource="gke_containerName.$POD_NAMESPACE.$POD_NAME.$MONITORED_CONTAINER_NAME"
 
-elif [[ ! -z "$container_id" ]]; then
-    monitored_resource="container.$container_id"
-
-elif [[ -e /var/run/docker.sock ]]; then
+elif [[ "$LOOKUP_GCE_INSTANCE_ID" == "true" ]]; then
     monitored_resource=$(curl --silent -f -H 'Metadata-Flavor: Google' http://169.254.169.254/computeMetadata/v1/instance/id 2>/dev/null)
+
+elif [[ ! -z "$MONITORED_CONTAINER_NAME" ]]; then
+    monitored_resource="containerName.$MONITORED_CONTAINER_NAME"
+
 fi
 
-if [[ -z "$monitored_resource" ]]; then
-    echo "Failed to determine the monitored resource."
-    exit 1
-fi
-
-sed -i "s/%HOSTNAME%/$monitored_resource/" "$configuration_file"
+sed -i "s/%MONITORED_RESOURCE%/$monitored_resource/" "$configuration_file"
 
 mount -a && \
     cp -r /etc/* /host/etc/ && \
