fa040249286bc870c0fbe7948c940b9fe55d45b2
Igor Peshansky <igorp@google.com>
2019-10-22 13:33:35 -0400

    Be verbose when running prepare.sh.

--- prepare.sh
+++ prepare.sh
@@ -3,4 +3,5 @@ if [[ "${PKGFORMAT-}" != "deb" && "${PKGFORMAT-}" != "rpm" ]]; then
   echo >&2 "This script requires PKGFORMAT to be either 'deb' or 'rpm'"
   exit 1
 fi
+set -x
 cp collectd.conf "${PKGFORMAT}"
